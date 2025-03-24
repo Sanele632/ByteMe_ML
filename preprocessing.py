2c2fe15cdd1f5a14dd8040f7ad476a286e25122b
Sanele632 <142526625+Sanele632@users.noreply.github.com>
2025-03-24 18:07:22 -0500

    Create preprocessing.py

---
+++ preprocessing.py
@@ -0,0 +1,87 @@
+#Version: v0.1
+#Date Last Updated: 3/24/25
+
+
+#%% MODULE BEGINS
+module_name = '<***>'
+
+'''
+Version: <***>
+
+Description:
+    <***>
+
+Authors:
+    Sanele Harmon, Taeden Kitchens
+
+Date Created     :  3/24/25
+Date Last Updated:  3/24/25
+
+Doc:
+    PA1
+
+Notes:
+    Preprocessing
+'''
+
+#%% IMPORTS                    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+if __name__ == "__main__":
+   import os
+   #os.chdir("./../..")
+#
+
+#custom imports
+
+
+#other imports
+from   copy       import deepcopy as dpcpy
+
+'''
+from   matplotlib import pyplot as plt
+import mne
+import numpy  as np 
+import os
+import pandas as pd
+import seaborn as sns
+'''
+#%% USER INTERFACE              ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+#%% CONSTANTS                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+#%% CONFIGURATION               ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+#%% INITIALIZATIONS             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+#%% DECLARATIONS                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#Global declarations Start Here
+
+
+
+#Class definitions Start Here
+
+
+
+#Function definitions Start Here
+def main():
+    pass
+#
+
+#%% MAIN CODE                  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Main code start here
+
+
+
+#%% SELF-RUN                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Main Self-run block
+if __name__ == "__main__":
+    
+    print(f"\"{module_name}\" module begins.")
+    
+    #TEST Code
+    main()
